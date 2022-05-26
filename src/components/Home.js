77ac95b504b93c14613aba0ac6cbba74138763e9
Tanner Barcelos <tanner.m.barcelos@gmail.com>
2022-05-25 19:56:37 -0700

    Changing email

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,39 +4,39 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="section_container" id="home">
-      <div className="hero-content">
-        <div className="headline">
+    <div className='section_container' id='home'>
+      <div className='hero-content'>
+        <div className='headline'>
           <h1>Hi, I'm Tanner</h1>
           <span>Software Engineer. Researcher. Computer Scientist.</span>
         </div>
-        <div className="socials">
-          <a href="https://github.com/TannerBarcelos" target="_blank">
-            <i className="fab fa-github"></i>
+        <div className='socials'>
+          <a href='https://github.com/TannerBarcelos' target='_blank'>
+            <i className='fab fa-github'></i>
           </a>
 
           <a
-            href="https://www.linkedin.com/in/tanner-barcelos-695619a1/"
-            target="_blank"
+            href='https://www.linkedin.com/in/tanner-barcelos-695619a1/'
+            target='_blank'
           >
-            <i className="fab fa-linkedin"></i>
+            <i className='fab fa-linkedin'></i>
           </a>
 
           <a
-            href="https://drive.google.com/file/d/1r_jZFhB_8bDX1FfAHpfg6mF-cN-c_VZC/view?usp=sharing"
-            target="_blank"
+            href='https://drive.google.com/file/d/1r_jZFhB_8bDX1FfAHpfg6mF-cN-c_VZC/view?usp=sharing'
+            target='_blank'
           >
-            <i className="far fa-file"></i>
+            <i className='far fa-file'></i>
           </a>
 
-          <a href="https://medium.com/@tanner.manuel.barcelos" target="_blank">
-            <i class="fab fa-medium"></i>
+          <a href='https://medium.com/@tanner.manuel.barcelos' target='_blank'>
+            <i class='fab fa-medium'></i>
           </a>
         </div>
         <a
-          className="contact-me-cta"
-          href="mailto:tanner.m.barcelos@gmail.com?subject=Reaching Out"
-          target="_blank"
+          className='contact-me-cta'
+          href='mailto:barcelos.m.tanner@gmail.com?subject=Reaching Out'
+          target='_blank'
         >
           Contact Me
         </a>
