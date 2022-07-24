9daea1ba4db7fc68b2907150370bd9bc82208cb2
Tanner Barcelos <tanner.m.barcelos@gmail.com>
2022-07-24 16:02:26 -0700

    Design changes done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,32 +5,8 @@ const Home = () => {
     <div className='section_container' id='home'>
       <div className='hero-content'>
         <div className='headline'>
-          <h1>Hi, I'm Tanner</h1>
-          <span>Senior Software Engineer at Visa | Creator</span>
-        </div>
-        <div className='socials'>
-          <a
-            href='https://github.com/TannerBarcelos'
-            target='_blank'
-            title='GitHub'
-          >
-            <i className='fab fa-github'></i>
-          </a>
-
-          <a
-            href='https://www.linkedin.com/in/tanner-barcelos-695619a1/'
-            target='_blank'
-            title='LinkedIn'
-          >
-            <i className='fab fa-linkedin'></i>
-          </a>
-          <a
-            href='https://medium.com/@tannerbarcelos'
-            target='_blank'
-            title='Blog'
-          >
-            <i class='fab fa-medium'></i>
-          </a>
+          <h1>Hi,I'm Tanner</h1>
+          <span>Senior Full Stack Software Engineer at Visa</span>
         </div>
         <div className='contact-container'>
           <a
@@ -48,6 +24,43 @@ const Home = () => {
             Resume
           </a>
         </div>
+      </div>
+      <div className='socials'>
+        <a
+          href='https://github.com/TannerBarcelos'
+          target='_blank'
+          title='GitHub Profile'
+        >
+          <i className='fab fa-github'></i>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/tanner-barcelos-695619a1/'
+          target='_blank'
+          title='LinkedIn Profile'
+        >
+          <i className='fab fa-linkedin'></i>
+        </a>
+        <a
+          href='https://www.youtube.com/channel/UCce_URD7JVNz9R4WuZH1X3g'
+          target='_blank'
+          title='Youtube Channel'
+        >
+          <i class='fab fa-youtube'></i>
+        </a>
+        <a
+          href='https://tanner.hashnode.dev/'
+          target='_blank'
+          title='Hashnode Blog'
+        >
+          <i class='fab fa-hashnode'></i>
+        </a>
+        <a
+          href='https://twitter.com/dev_with_tanner'
+          target='_blank'
+          title='Technical Twitter Profile'
+        >
+          <i class='fab fa-twitter'></i>
+        </a>
       </div>
     </div>
   );
